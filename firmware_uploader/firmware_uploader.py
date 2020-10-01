1ac6fc68110762f5226739fd443b3597ceb368c1
Peter Polidoro <peterpolidoro@gmail.com>
2020-10-01 16:29:40 -0400

    Add local URL

--- firmware_uploader/firmware_uploader.py
+++ firmware_uploader/firmware_uploader.py
@@ -26,6 +26,39 @@ class FirmwareUploader(object):
 
     Example Usage:
 
+    fu -e teensy40 -d https://github.com/janelia-arduino/YArenaValveController "(/dev/ttyACM)[0-2]"
+    # Environment: teensy40
+    # Dry Run: True
+    # Firmware URL: https://github.com/janelia-arduino/YArenaValveController
+    # Upload ports: ['/dev/ttyACM0', '/dev/ttyACM1', '/dev/ttyACM2']
+    # Do you want to continue? [y/N]: y
+    # Cloning into 'YArenaValveController'...
+    #remote: Enumerating objects: 118, done.
+    # remote: Counting objects: 100% (118/118), done.
+    # remote: Compressing objects: 100% (78/78), done.
+    # remote: Total 118 (delta 65), reused 88 (delta 35), pack-reused 0
+    # Receiving objects: 100% (118/118), 19.79 KiB | 2.47 MiB/s, done.
+    # Resolving deltas: 100% (65/65), done.
+    # ['stty', '-F', '/dev/ttyACM0', '134']
+    # ['pio', 'run', '-e', 'teensy40', '--target', 'upload', '--upload-port', '/dev/ttyACM0']
+    # ['stty', '-F', '/dev/ttyACM1', '134']
+    # ['pio', 'run', '-e', 'teensy40', '--target', 'upload', '--upload-port', '/dev/ttyACM1']
+    # ['stty', '-F', '/dev/ttyACM2', '134']
+    # ['pio', 'run', '-e', 'teensy40', '--target', 'upload', '--upload-port', '/dev/ttyACM2']
+
+    fu -e teensy40 https://github.com/janelia-arduino/YArenaValveController "(/dev/ttyACM)[0-2]"
+    # Environment: teensy40
+    # Dry Run: False
+    # Firmware URL: https://github.com/janelia-arduino/YArenaValveController
+    # Upload ports: ['/dev/ttyACM0', '/dev/ttyACM1', '/dev/ttyACM2']
+    # Do you want to continue? [y/N]:
+
+    fu -e teensy40 ./git/arduino/YArenaValveController/ "(/dev/ttyACM)[0-2]"
+    # Environment: teensy40
+    # Dry Run: False
+    # Firmware URL: ./git/arduino/YArenaValveController/
+    # Upload ports: ['/dev/ttyACM0', '/dev/ttyACM1', '/dev/ttyACM2']
+    # Do you want to continue? [y/N]:
     '''
 
     def __init__(self,environment,dry_run,firmware_url,upload_ports,*args,**kwargs):
@@ -40,26 +73,34 @@ class FirmwareUploader(object):
         if not self.dry_run:
             subprocess.run(args)
         else:
+            print(os.getcwd())
             print(args)
 
+    def _upload(self):
+        for upload_port in self.upload_ports:
+            if self.environment is not None:
+                if 'teensy' in self.environment:
+                    # teensy loader ignores --upload-port, so must manually
+                    # put into bootloader mode by setting baud of port
+                    # before uploading
+                    self._output(['stty','-F',upload_port,'134'])
+                self._output(['pio','run','-e',self.environment,'--target','upload','--upload-port',upload_port])
+            else:
+                self._output(['pio','run','--target','upload','--upload-port',upload_port])
+
     def run(self):
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            tmpdirpath = Path(tmpdirname)
-            os.chdir(tmpdirpath)
-            subprocess.run(['git','clone',self.firmware_url],check=True)
-            repository_name = Path(self.firmware_url).name
-            tmpdirpath /= repository_name
-            os.chdir(tmpdirpath)
-            for upload_port in self.upload_ports:
-                if self.environment is not None:
-                    if 'teensy' in self.environment:
-                        # teensy loader ignores --upload-port, so must manually
-                        # put into bootloader mode by setting baud of port
-                        # before uploading
-                        self._output(['stty','-F',upload_port,'134'])
-                    self._output(['pio','run','-e',self.environment,'--target','upload','--upload-port',upload_port])
-                else:
-                    self._output(['pio','run','--target','upload','--upload-port',upload_port])
+        if Path(self.firmware_url).exists():
+            os.chdir(self.firmware_url)
+            self._upload()
+        else:
+            with tempfile.TemporaryDirectory() as tmpdirname:
+                tmpdirpath = Path(tmpdirname)
+                os.chdir(tmpdirpath)
+                subprocess.run(['git','clone',self.firmware_url],check=True)
+                repository_name = Path(self.firmware_url).name
+                tmpdirpath /= repository_name
+                os.chdir(tmpdirpath)
+                self._upload()
 
 
 @click.command()
