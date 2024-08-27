e7ad9adef34e4b273734513cf03126b21c536d41
Peter Polidoro <peter@polidoro.io>
2024-08-27 16:13:36 -0400

    janelia-pypi -> janelia-python

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/janelia-pypi/firmware_uploader_python',
+    url='https://github.com/janelia-python/firmware_uploader_python',
 
     # Author details
     author='Peter Polidoro',
